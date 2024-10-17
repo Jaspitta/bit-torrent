4e223dca85165aa179fa6836d78ee0b6ceede2b5
Jaspitta <ste.scarpitta@gmail.com>
2024-10-18 00:17:20 +0200

    total rework, change to pass the index as reference to every decoding method, add dictionary case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import com.google.gson.Gson;
+
+import java.io.UnsupportedEncodingException;
 import java.lang.Character;
 import java.util.ArrayList;
 import java.util.List;
-// import com.dampcake.bencode.Bencode; - available if you need it!
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
   private static final Gson gson = new Gson();
@@ -11,84 +14,115 @@ public class Main {
     String command = args[0];
     if("decode".equals(command)) {
       String bencodedValue = args[1];
-      System.out.println(gson.toJson(new Main().new DecodedMessage(bencodedValue).decodeMessage()));
+      var index = new Main().new Reference<Integer>(0);
+      System.out.println(gson.toJson(decodeMessage(bencodedValue, index)));
     } else {
       System.out.println("Unknown command: " + command);
     }
 
   }
 
-  public class DecodedMessage {
-    private String originalMessage;
-    private Integer end;
-
-    public DecodedMessage(String message){
-      this.originalMessage = message;
-    }
-
-    Object decodeMessage(){
-      switch(this.originalMessage){
-        case String message when Character.isDigit(message.charAt(0)) ->{
-          return decodeBencodeString(message);
+  public static Object decodeMessage(String bencodedString, Reference<Integer> index){
+      switch(bencodedString){
+        case String message when Character.isDigit(message.charAt(index.getValue())) -> {
+          return decodeBencodeString(message, index);
         }
-        case String message when message.charAt(0) == 'i' ->{
-          return decodeBencodeNumber(message);
+        case String message when message.charAt(index.getValue()) == 'i' -> {
+          return decodeBencodeNumber(bencodedString, index);
         }
-        case String message when message.charAt(0) == 'l' ->{
-          return decodeBencodeList(message);
+        case String message when message.charAt(index.getValue()) == 'l' -> {
+          return decodeBencodeList(bencodedString, index);
         }
-        default ->{
+        case String message when message.charAt(index.getValue()) == 'd' -> {
+          return decodeBencodeDictionary(bencodedString, index);
+        }
+        default -> {
           throw new RuntimeException("Unsupported format");
         }
       }
-    }
-
-    String decodeBencodeString(String bencodedString) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) {
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
-        }
-      }
-      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      this.end = firstColonIndex+length;
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-    }
-
-    Long decodeBencodeNumber(String bencodedString) {
-      this.end = bencodedString.indexOf('e');
-      return Long.valueOf(bencodedString.substring(1, this.end));
-    }
-
-    List<Object> decodeBencodeList(String bencodeString){
-      if(bencodeString == null || bencodeString.length() < 3) return new ArrayList<Object>();
-
-      var messageCopy = bencodeString.substring(1);
-      var resp = new ArrayList<Object>();
-      while(messageCopy.length() > 1 && messageCopy.charAt(0) != 'e'){
-        switch(messageCopy){
-          case String messageTemp when Character.isDigit(messageTemp.charAt(0)) ->{
-            resp.add(decodeBencodeString(messageTemp));
-            messageCopy = messageTemp.substring(this.end+1);
-          }
-          case String messageTemp when messageTemp.charAt(0) == 'i' ->{
-            resp.add(decodeBencodeNumber(messageTemp));
-            messageCopy = messageTemp.substring(this.end+1);
-          }
-          case String messageTemp when messageTemp.charAt(0) == 'l' ->{
-            resp.add(decodeBencodeList(messageTemp));
-            messageCopy = messageTemp.substring(this.end + 1);
-          }
-          default ->{
-            throw new RuntimeException("Unsupported format");
-          }
-        }
-      }
-      this.end = bencodeString.length() - messageCopy.length();
-      return resp;
-    }
-
   }
 
+  public static String decodeBencodeString(String bencodedString, Reference<Integer> index) {
+    assert index.getValue() < bencodedString.length() - 1;
+    if(bencodedString == null || bencodedString.isEmpty()) return bencodedString;
+
+    int firstColonIndex = bencodedString.indexOf(':', index.getValue());
+    int end = firstColonIndex + Integer.parseInt(bencodedString.substring(index.getValue(), firstColonIndex));
+    index.setValue(end);
+    return bencodedString.substring(
+      firstColonIndex + 1,
+      end + 1 //exclusive, strings have no end character
+    );
+  }
+
+  public static Long decodeBencodeNumber(String bencodedString, Reference<Integer> index) {
+    assert index.getValue() < bencodedString.length() - 1;
+    if(bencodedString == null || bencodedString.isEmpty()) return null;
+
+    int end = bencodedString.indexOf('e', index.getValue());
+    int begin = index.getValue() + 1; //skip the i
+    index.setValue(end);
+
+    return Long.valueOf(bencodedString.substring(begin, end));
+  }
+
+  public static List<Object> decodeBencodeList(String bencodedString, Reference<Integer> index){
+    assert index.getValue() < bencodedString.length() - 1;
+    if(bencodedString == null) return null;
+    if(bencodedString.length() < 3) return new ArrayList<Object>();
+
+    var decodedList = new ArrayList<Object>();
+
+    while(index.getValue() + 2 < bencodedString.length() && bencodedString.charAt(index.getValue() + 1) != 'e'){
+      index.setValue(index.getValue()+1); // move to the begin of the next element, assuming we left off at the last
+      decodedList.add(decodeMessage(bencodedString, index));
+    }
+    index.setValue(index.getValue() + 1); // move to the next, for recursive calls and such, basically on the ending e
+    return decodedList;
+  }
+
+  public static Map<String, Object> decodeBencodeDictionary(String bencodedString, Reference<Integer> index) {
+    assert index.getValue() < bencodedString.length() - 1;
+
+    if(bencodedString == null) return null;
+    if(bencodedString.isEmpty()) return new HashMap<String, Object>();
+
+    var decodedDic = new HashMap<String, Object>();
+    while(index.getValue() + 2 < bencodedString.length() && bencodedString.charAt(index.getValue() + 1) != 'e'){
+      index.setValue(index.getValue()+1); // move to the begin of the next element, assuming we left off at the last
+      // TODO: check the next is a string?
+      // TODO: think if there is some optimization that can be done here since we knwo the next is a string;
+      var k = (String)decodeMessage(bencodedString, index);
+      index.setValue(index.getValue() + 1); // moving to the next element
+      var v = decodeMessage(bencodedString, index);
+      decodedDic.put(k, v);
+    }
+
+    return decodedDic;
+  }
+
+  /**
+   * Used to keep track of the start/end index of the element we are decoding.
+   * The goal is to move the index but never actually modify the original string.
+   * At the beginning of a decoding method the value should be at the first index of the element we are decoding.
+   * At the end of the decoding method the value should be at the last index of the element we are decoding.
+   *
+   * In general:
+   *  this can also be used to pass a reference to anything when the pass by value policy of java is not what we need
+   */
+  private class Reference<T>{
+    private T value;
+
+    Reference(T value){
+      this.value = value;
+    }
+
+    public void setValue(T value){
+      this.value = value;
+    }
+
+    public T getValue(){
+      return this.value;
+    }
+  }
 }
---
+++ test_case.sh
@@ -0,0 +1,5 @@
+mvn clean package
+for input in "lli617e9:blueberryee", "lli4eei5ee", "l6:bananai781ee", "lli781e6:bananaee", "i4294967300e", "d3:foo3:bar5:helloi52ee"
+do
+	java -jar ./target/java_bittorrent.jar "decode" "$input"
+done
